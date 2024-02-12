6ba5da3210dbe823c6e3daccc97058732b36d3d1
Roberto marcio Braga Junior <Roberto.junior@intranet.local>
2024-02-12 15:48:33 -0300

    corrigindo nivel atual com clock de intervalo

--- count/core/views.py
+++ count/core/views.py
@@ -178,16 +178,25 @@ def nivel_atual(request, torneio_id):
 
     tempo_decorrido = timezone.now() - torneio.horario_inicio
     minutos_decorridos = tempo_decorrido.total_seconds() / 60
-    
+        
     # Calcular o número do nível com base no tempo decorrido e na duração de cada nível
     if torneio.intervalo_ativo:
-        duracao_nivel = torneio.tempo_blind_preintervalo  # Duração do nível após o intervalo
-    else:
-        duracao_nivel = torneio.tempo_blind_posintervalo  # Duração do nível antes do intervalo
+        duracao_nivel = torneio.tempo_blind_posintervalo  # Duração do nível após o intervalo
+        minutosposintervalo = timezone.now() - torneio.horario_intervalo
+        minutosposintervalo = minutosposintervalo.total_seconds() / 60
         
+        niveisantes = torneio.horario_intervalo - torneio.horario_inicio
+        niveisantes = niveisantes.total_seconds() / 60
+        
+        # Calcular quantos níveis aconteceram desde o início do torneio até o intervalo com o blind pré-intervalo
+        niveis_antes_intervalo = int(niveisantes / torneio.tempo_blind_preintervalo)
+        print(niveis_antes_intervalo)
+        # Calcular o número do nível considerando o tempo decorrido após o intervalo, desconsiderando o tempo de intervalo
+        numero_nivel = niveis_antes_intervalo + int(minutosposintervalo / duracao_nivel)
+    else:
+        duracao_nivel = torneio.tempo_blind_preintervalo  # Duração do nível antes do intervalo
+        numero_nivel = int(minutos_decorridos / duracao_nivel) + 1
 
-    numero_nivel = int(minutos_decorridos / duracao_nivel) + 1
-    
     # Obter o nível correspondente ao número calculado
     try:
         nivel = Level.objects.get(nivel=numero_nivel)
